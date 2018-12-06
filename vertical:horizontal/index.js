df915a523972f8ca2fec8564a20a1b571e4d4d93
DaveMitten <davidmitten88@gmail.com>
2018-12-06 11:09:49 +0000

    added swipe right button func, made borders work

--- vertical:horizontal/index.js
+++ vertical:horizontal/index.js
@@ -4,25 +4,28 @@ window.onload = () => {
     pagination: {
       el: '.swiper-pagination-h',
       clickable: true
-    }
+    },
+    navigation: {
+      nextEl: '.iconSize',
+      clickable: true,
+      // prevEl: '.swiper-button-prev',
+    },
   });
+
+
   var swiperV = new Swiper('.swiper-container-v', {
     direction: 'vertical',
     spaceBetween: 10,
     pagination: {
       el: '.swiper-pagination-v',
       clickable: true,
-      noSwipingClass: true
-
+      // noSwipingClass: true,
     }
+    // navigation: {
+    //   prevEl: '.clickUp',
+    //   clickable: true,
+    //   // prevEl: '.swiper-button-prev',
+    // },
   });
-  var swiperVTwo = new Swiper('.swiper-container-v-two', {
-    direction: 'vertical',
-    spaceBetween: 10,
-    pagination: {
-      el: '.swiper-pagination-v',
-      clickable: true
-    },
-  });
-};
-e
+
+}
