f5b1de4950edee0156055f896e6a7c2547dff716
DaveMitten <davidmitten88@gmail.com>
2018-12-06 18:16:58 +0000

    most up to date versions of currently owkring monetate exp

--- vertical:horizontal/index.js
+++ vertical:horizontal/index.js
@@ -1,4 +1,22 @@
+
+  // window.addEventListener('resize', () => {
+  //   // We execute the same script as before
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // });
+
+
+
 window.onload = () => {
+
+
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+
   var swiperH = new Swiper('.swiper-container-h', {
     spaceBetween: 20,
     pagination: {
@@ -28,4 +46,12 @@ window.onload = () => {
     // },
   });
 
+
+
 }
+
+// window.addEventListener('resize', () => {
+//   // We execute the same script as before
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
